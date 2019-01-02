103927fbe8caf5e1a3c44081ec7facc29033b688
Zev Spilman <zevspilman@yahoo.com>
2019-01-02 15:35:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_arr = []
+  array.each {|thing| new_arr.push(badge_maker(thing))}
+  return new_arr
+  end
+
+def assign_rooms(array)
+  new_arr = []
+  array.each {|thing| new_arr.push( "Hello, #{thing}! You'll be assigned to room #{array.index(thing) + 1}!")}
+  return new_arr
+end
+
+def printer(array)
+  amt = 0
+ loop do 
+   if amt < array.size
+     
+  
+end
