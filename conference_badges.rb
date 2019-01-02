9a93f03b35737f05c296e29a934e6445d8e5cb2a
Zev Spilman <zevspilman@yahoo.com>
2019-01-02 15:43:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,5 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(array)
-  assign_rooms(array)
+  array.each { |thing2| batch_badge_creator(array[thing2]) assign_rooms(array[thing2])}
 end
